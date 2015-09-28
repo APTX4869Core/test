e182b19446e32f23010e009eaa51ab9c85e763c4
APTX4869 <962592265@qq.com>
2015-09-28 17:45:01 +0800

    add class2 & transportation controller

---
+++ application/controllers/transportation.php
@@ -0,0 +1,19 @@
+<?php
+	//计算运费
+	class Transportation extension CI_controller{
+		//将快递公司及目的省份转为系数
+		public function getdistance(){
+			
+			
+		}
+		//将货品明细转为重量
+		public function getmass(){
+			
+			
+		}
+		//计算得到运费
+		public function getexpences(){
+			
+			
+		}
+	}
